499ff312dd662d77571f21999f6a129b2babaafd
Mj444555 <106246063+Mahi444555@users.noreply.github.com>
2022-12-31 12:45:14 +0530

    Component Navbar Card Caraousal Footer...

--- food-ordering-app/mernapp/src/components/Caraousal.js
+++ food-ordering-app/mernapp/src/components/Caraousal.js
@@ -6,7 +6,7 @@ function Caraousal() {
       <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" style={{objectFit:"contain !important"}}>
         
         <div class="carousel-inner" id='carousel'>
-        <div class="carousel-caption" style={{zIndex:"10"}}>
+        <div class="carousel-caption" style={{zIndex:"10"}}> 
               <form class="d-flex">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                 <button class="btn btn-outline-success text-white bg-success" type="submit">Search</button>
